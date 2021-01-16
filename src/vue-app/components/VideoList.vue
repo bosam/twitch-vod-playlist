94b71f541533d403fad3a0a7abc5853409828601
bosam <supertop>
2021-01-15 20:14:26 -0800

    Added @ahmadnassri/spawn-promise library to handle better control of the spawned process + added visual indicator when link has been clicked and alerts via a toast when the process returns an error + added copy to clipboard feature copying the video's url

--- src/vue-app/components/VideoList.vue
+++ src/vue-app/components/VideoList.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h6 v-if="list.length">
-      {{ list.length }} videos ( {{ channelNameProcessed }} )
+      {{ list.length }} videos ( {{ channelNameProcessed }} ) <font-awesome-icon
+        v-if="spawningVideo"
+        icon="spinner"
+        spin
+      />
     </h6>
 
     <ul class="fa-ul">
@@ -12,6 +16,13 @@
         <span class="fa-li">
           <font-awesome-icon icon="angle-right" />
         </span>
+        <font-awesome-icon
+          style="cursor: hand"
+          icon="copy"
+          :title="video.url"
+          @click="copyToClipboard(video.url)"
+        />
+        |
         <div class="float-right small">
           <span
             :title="createdAtRaw(video.created_at)"
@@ -29,53 +40,70 @@
 
 <script type="ts">
 /* eslint-disable indent */
-import Vue from 'vue';
-import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-import { format, utcToZonedTime } from 'date-fns-tz';
-import SpawnService from '../services/spawn.service';
+  import Vue from 'vue';
+  import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+  import { format, utcToZonedTime } from 'date-fns-tz';
+  import SpawnService from '../services/spawn.service';
 
-const currentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const currentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-export default Vue.extend({
-  name: 'VideoList',
-  props: [
-    'list',
-    'channelName'
-  ],
-  data: () => {
-    return {
-      channelNameProcessed: ''
-    };
-  },
-  watch: {
-    list: function () {
-      this.channelNameProcessed = this.channelName;
+  export default Vue.extend({
+    name: 'VideoList',
+    props: [
+      'list',
+      'channelName'
+    ],
+    data: () => {
+      return {
+        channelNameProcessed: '',
+        spawningVideo: false
+      };
     },
-  },
-  methods: {
-    parseTitle(video) {
-      let title = video.title;
-      if ('Bibi300' === video.user_name &&
-        -1 !== title.indexOf('|')) {
-        title = title.split('|')[1];
+    watch: {
+      list: function () {
+        this.channelNameProcessed = this.channelName;
+      },
+    },
+    methods: {
+      parseTitle(video) {
+        // Find way to have title processors
+        return video.title;
+      },
+      createdAtRaw(createdAt) {
+        createdAt = utcToZonedTime(createdAt, currentTimezone);
+        return format(createdAt, 'yyyy-MM-dd HH:mm:ss zzz');
+      },
+      createdAtFromNow(createdAt) {
+        createdAt = utcToZonedTime(createdAt, currentTimezone);
+        return formatDistanceToNow(createdAt, { addSuffix: true });
+      },
+      spawnMedia(url, e) {
+        e.preventDefault();
+        this.spawningVideo = true;
+        // Artificial timeout - since spawning won't return the buffer until the process is closed
+        setTimeout(() => { this.spawningVideo = false; }, 2500);
+        SpawnService.spawnMedia(url)
+          .catch(error => {
+            this.$bvToast.toast(error.stdout.toString(), {
+              title: 'Error',
+              variant: 'danger',
+              solid: false
+            });
+        }).finally(() => {
+          // In case it returns early due to an error
+          this.spawningVideo = false;
+        });
+      },
+      copyToClipboard(url) {
+        navigator.clipboard.writeText(url);
+        this.$bvToast.toast('Url copied successfully!', {
+          title: 'Success',
+          variant: 'success',
+          solid: false
+        });
       }
-
-      return title;
-    },
-    createdAtRaw(createdAt) {
-      createdAt = utcToZonedTime(createdAt, currentTimezone);
-      return format(createdAt, 'yyyy-MM-dd HH:mm:ss zzz');
-    },
-    createdAtFromNow(createdAt) {
-      createdAt = utcToZonedTime(createdAt, currentTimezone);
-      return formatDistanceToNow(createdAt, { addSuffix: true });
-    },
-    spawnMedia(url, e) {
-      e.preventDefault();
-      SpawnService.spawnMedia(url);
     }
-  }
-});
+  });
 </script>
 
 <style>
--- src/vue-app/services/spawn.service.ts
+++ src/vue-app/services/spawn.service.ts
@@ -1,24 +1,25 @@
-import process from 'child_process';
+import spawn from '@ahmadnassri/spawn-promise';
 import SettingsService from './settings.service';
 import { sprintf } from 'sprintf-js';
-import { StringDecoder } from 'string_decoder';
+// import { StringDecoder } from 'string_decoder';
 
-const decoder = new StringDecoder('utf-8');
+// const decoder = new StringDecoder('utf-8');
 
 class SpawnService {
-  spawnMedia(url) {
-    SettingsService.load().then(settings => {
-      const scriptUrl = sprintf('%s %s', settings.paths.script, url);
-      console.info('running: ' + scriptUrl);
-      const cmd = process.spawn('cmd', ['/c', scriptUrl]);
-      cmd.stdout.on('data', (data) => {
-        const decoded = decoder.write(data);
-        console.info(decoded);
-        if (-1 !== decoded.indexOf('could not be found.')) {
-          console.error('Could not find', url);
-        }
+  async spawnMedia(url: string) : Promise<string> {
+    return SettingsService.load()
+      .then(settings => {
+        const scriptUrl = sprintf('%s %s', settings.paths.script, url);
+        console.info('running: ' + scriptUrl);
+        return spawn('cmd', ['/c', scriptUrl]);
+        // cmd.stdout.on('data', (data) => {
+        //   const decoded = decoder.write(data);
+        //   console.info(decoded);
+        //   if (-1 !== decoded.indexOf('could not be found.')) {
+        //     console.error('Could not find', url);
+        //   }
+        // });
       });
-    });
   }
 }
 
